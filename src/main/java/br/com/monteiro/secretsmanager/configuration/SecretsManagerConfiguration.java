8a353a1a4353f93ee164b9c73ae404dc43795351
Flavio Monteiro <flaviohnm@gmail.com>
2024-07-21 22:57:16 -0300

    :rocket: feat: add secretmanager configuration and controller

--- src/main/java/br/com/monteiro/secretsmanager/configuration/SecretsManagerConfiguration.java
+++ src/main/java/br/com/monteiro/secretsmanager/configuration/SecretsManagerConfiguration.java
@@ -1,10 +1,12 @@
 package br.com.monteiro.secretsmanager.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties
 @EnableConfigurationProperties
 public class SecretsManagerConfiguration {
 
