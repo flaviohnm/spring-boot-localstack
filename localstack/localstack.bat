7f512f4c038806776f6ff96562b01d17810bcee9
Flavio Monteiro <flaviohnm@gmail.com>
2024-07-23 09:03:14 -0300

    feat: adjust on createStack file

--- localstack/localstack.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo ### Criando Chaves no AWS Parameter Store do LocalStack...
-aws --endpoint http://localhost:4566 --profile localstack ssm put-parameter --name "/config/spring-boot-localstack_localstack/helloWorld" --value "Hello World Parameter Store" --type String
